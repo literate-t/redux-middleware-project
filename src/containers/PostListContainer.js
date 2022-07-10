9b6609403718ce44fd71b6239ffab46c35af9f86
literate <gaekoon@gmail.com>
2022-07-10 18:38:42 +0900

    rendered posts component

---
+++ src/containers/PostListContainer.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import PostList from '../components/PostList';
+import { getPosts } from '../modules/posts';
+
+function PostListContainer() {
+  const { data, loading, error } = useSelector((state) => state.posts.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]); // depth에 dispatch를 넣으나 안 넣으나 똑같음. 다만 eslint 규칙을 준수하기 위해
+
+  if (loading) return <div>로딩 중...</div>;
+  if (error) return <div>에러 발생!</div>;
+  if (!data) return null;
+
+  return <PostList posts={data} />;
+}
+
+export default PostListContainer;
